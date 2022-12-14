c21db94b370028961f132ea260d9a6a2873030ee
vishal <vsvishal11@yahoo.com>
2022-12-14 11:37:07 +0530

    Lambda API & dynamodb created

---
+++ infrastructure/SpaceStack.ts
@@ -0,0 +1,30 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { GenericTable } from "./GenericTable";
+
+export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
+  private spaceTable = new GenericTable("SapcesTable", "spaceId", this);
+
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
+
+    // Hello Api Lambda Integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
+  }
+}
