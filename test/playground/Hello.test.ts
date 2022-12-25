1f615899d286bc5472c5b530f5a0aaab798005db
vishal <vsvishal11@yahoo.com>
2022-12-25 23:09:34 +0530

    Read Debugger added

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,8 +1,12 @@
-import { handler } from "../../services/SpacesTable/Create";
+import { handler } from "../../services/SpacesTable/Read";
 
 const event = {
   body: {
     location: "Pune",
   },
 };
-handler(event as any, {} as any);
+
+const result = handler({} as any, {} as any).then((apiResult) => {
+  const items = JSON.parse(apiResult.body);
+  console.log(123);
+});
